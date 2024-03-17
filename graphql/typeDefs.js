e33302101d8304bd5a02e01313bfbacd0c4fd424
Hcaglar32 <hasan.49.50@hotmail.com>
2024-03-17 13:18:57 +0300

    Giriş ve kayıt işlemleri backend kodları tamamlandı.

--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -1,41 +1,43 @@
 const { gql } = require('apollo-server');
 
 module.exports = gql`
-    type Message{
+    type Message {
         text: String
         createdAt: String
         createdBy: String
     }
 
-    type User{
-        username:String 
-        email:String
-        password:String
-        token:String
+    type User {
+        username: String 
+        email: String
+        password: String
+        token: String
     }
 
-    input MessageInput{
-        text:String
-        username:String
+    input MessageInput {
+        text: String
+        username: String
     }
 
-    input RegisterInput{
-        username:String
-        email:String
-        password:String
+    input RegisterInput {
+        username: String
+        email: String
+        password: String
     }
 
-    input LoginInput{
-        email:String
-        password:String
+    input LoginInput {
+        email: String
+        password: String
     }
 
-    type Query{
-        message:(id:ID!): Message
+    type Query {
+        message(id: ID!): Message
+        user(id: ID!): User 
     }
 
-    type Mutation{
-        createMessage(messageInput:MessageInput):Message!
-        registerUser(registerInput:RegisterInput):User
+    type Mutation {
+        createMessage(messageInput: MessageInput): Message!
+        registerUser(registerInput: RegisterInput): User
+        loginUser(loginInput: LoginInput): User 
     }
-`
+`;
--- models/User.js
+++ models/User.js
@@ -3,7 +3,7 @@ const { model, Schema } = require('mongoose');
 const userSchema = new Schema({
     username: { type: String, default: null },
     email: { type: String, unique: true },
-    email: { type: String },
+    password: { type: String },
     token: { type: String }
 });
 
