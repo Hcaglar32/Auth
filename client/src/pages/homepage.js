ceab431296f6845f4c7596ba7aca86f7167d235a
Hcaglar32 <hasan.49.50@hotmail.com>
2024-04-08 17:37:35 +0300

    Front-end alanı tamamlandı çıkış işlemi yapıalcak ve jwt-decode hatası çözülecek

---
+++ client/src/pages/homepage.js
@@ -0,0 +1,6 @@
+function Homepage(){
+    return(
+        <h1>Burası Ana Sayfa</h1>
+    )
+}
+export default Homepage;
--- graphql/typeDefs.js
+++ graphql/typeDefs.js
@@ -2,31 +2,33 @@ const { gql } = require('apollo-server');
 
 module.exports = gql`
     type Message {
-        text: String
-        createdAt: String
+        text: String,
+        createdAt: String,
         createdBy: String
     }
 
     type User {
-        username: String 
-        email: String
-        password: String
+        username: String,
+        email: String,
+        password: String,
         token: String
     }
 
     input MessageInput {
-        text: String
+        text: String,
         username: String
     }
 
     input RegisterInput {
-        username: String
-        email: String
-        password: String
+        username: String,
+        email: String,
+        password: String,
+        confrimPassword: String,
+
     }
 
     input LoginInput {
-        email: String
+        email: String,
         password: String
     }
 
